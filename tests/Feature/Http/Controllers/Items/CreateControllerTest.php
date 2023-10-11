262f715f4aca565f9b42a5c4f2332cc2ebfe1dff
Aaron Klemm <klemm.aaron@gmail.com>
2023-10-11 11:53:04 -0400

    Generate Item create and store controllers w/ tests and add some defaults. Refactor ItemIndexController name and location.

---
+++ tests/Feature/Http/Controllers/Items/CreateControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Items;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreateControllerTest extends TestCase
+{
+    public function does_create_route_exist_and_load(): void
+    {
+        $response = $this->get('/items/create');
+
+        $response->assertStatus(200);
+    }
+}
